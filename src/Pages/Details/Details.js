3effe99438856b4fef383c03bf5d89e6b98cf5ff
IffatNur <iffatnoorshad00@gmail.com>
2022-12-11 23:57:24 +0600

    details page

---
+++ src/Pages/Details/Details.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useLoaderData } from 'react-router';
+
+const Details = () => {
+    const projects = useLoaderData();
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Details;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Details from "../Pages/Details/Details";
 import Home from "../Pages/Home";
 
 export const router = createBrowserRouter([
     {
         path:'/',
         element:<Home></Home>
+    },
+    {
+        path: '/project/:id',
+        element: <Details></Details>,
+        loader: () => fetch('projects.json')
     }
 ])
